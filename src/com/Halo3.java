fc47e81a92b93c2883d5d11bad3b6c24c378830b
nithinankar <nithin.ankar@gmail.com>
2022-05-21 18:11:55 +0530

    Adding new file to Halo Project

--- src/com/Halo3.java
+++ src/com/Halo3.java
@@ -4,7 +4,7 @@ public class Halo3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Wleocme to Git & Java");
+		System.out.println("Welcome to Git & Java");
 	}
 
 }
