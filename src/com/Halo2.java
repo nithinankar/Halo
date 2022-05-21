d2770875f4a041ce9b6c668153c3ee6263712da3
nithinankar <nithin.ankar@gmail.com>
2022-05-21 17:47:39 +0530

    Halo First Java Project

---
+++ src/com/Halo2.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Halo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Halo2!!!");
+	}
+
+}
---
+++ src/com/HaloWorld.java
@@ -0,0 +1,10 @@
+package com;
+
+public class HaloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Halo! Welcome to Java Programming!!");
+	}
+
+}
